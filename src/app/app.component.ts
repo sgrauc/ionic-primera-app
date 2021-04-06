dd22f57ddd2a9005abad2cc69b0adca357ed7556
Sergio Grau <sergiograucecilia@gmail.com>
2021-04-06 16:57:40 +0200

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: 'app.component.html',
   styleUrls: ['app.component.scss'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   constructor() {}
+
+ngOnInit (){
+  let numero = 10;
+  // debugger;
+  numero = 20;
+  numero = numero +10;
+  numero = 0;
+  console.log(">>numero: " + numero);
+}
+
 }
